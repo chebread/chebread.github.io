c9d438386c7248dd7c398378f651a56214dfd6fe
chebread <83535893+chebread@users.noreply.github.com>
2026-08-22 14:41:03 +0900

    feat: Support date format (RFC3339)

--- lib/format_date_korean.go
+++ lib/format_date_korean.go
@@ -1,16 +1,23 @@
 package lib
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
+// FormatDateKorean은 다음 형식을 모두 처리합니다:
+//   - "2026-08-22"
+//   - "2026-08-22T14:32:23"
+//   - "2026-08-22T14:32:23+09:00" (RFC3339)
 func FormatDateKorean(dateString string) (string, error) {
-	inputLayout := "2006-01-02"
-
-	t, err := time.Parse(inputLayout, dateString)
-	if err != nil {
-		return "", err
+	if len(dateString) < 10 {
+		return "", fmt.Errorf("날짜 형식 오류: %q", dateString)
 	}
 
-	outputLayout := "2006년 01월 02일"
+	t, err := time.Parse("2006-01-02", dateString[:10])
+	if err != nil {
+		return "", fmt.Errorf("날짜 파싱 실패: %q: %w", dateString, err)
+	}
 
-	return t.Format(outputLayout), nil
+	return t.Format("2006년 01월 02일"), nil
 }
